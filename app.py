06b3321a4e26f97d1a64256baf4a3bcf4d5692c3
Kazi Khalid Hasan Tamim <113050909+kazikhalid757@users.noreply.github.com>
2023-07-29 13:21:53 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+
 # Sample data: Replace this with your actual dataset
 data = pd.read_csv("World_Muslim_Population _Dataset.csv")
-# Function to calculate the 5-year differences
-def calculate_5_year_differences(row):
-    return [row[i+5] - row[i] if i+5 < len(row) else None for i in range(len(row))]
 
 # Function to create the line chart for a selected country
 def plot_growth_rate(country_name):
     country_data = data[data['Country Name'] == country_name].values[0]
     years = data.columns[2:-1]
     growth_rate = country_data[2:-1]
-    five_year_diff = calculate_5_year_differences(growth_rate)
     
     plt.figure(figsize=(10, 6))
-    plt.plot(years, growth_rate, marker='o', label='Growth Rate')
-    plt.plot(years[4:], five_year_diff[4:], marker='o', label='5-Year Difference')
+    plt.plot(years, growth_rate, marker='o')
     plt.xlabel('Year')
     plt.ylabel('Growth Rate')
     plt.title(f'Growth Rate for {country_name}')
     plt.xticks(rotation=45)
-    plt.legend()
+
+    # Annotate each data point with its growth rate value
+    for x, y in zip(years, growth_rate):
+        plt.annotate(f'{y:.2f}', (x, y), textcoords="offset points", xytext=(0, 10), ha='center')
+
     st.pyplot(plt)
 
 # Main Streamlit app
