0ae5ecaced423668373ac1772347f42bd62b7667
Kazi Khalid Hasan Tamim <113050909+kazikhalid757@users.noreply.github.com>
2023-07-29 12:42:33 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,38 @@
+import streamlit as st
 import pandas as pd
-import bar_chart_race as bcr
+import matplotlib.pyplot as plt
 
+# Function to calculate the 5-year differences
+def calculate_5_year_differences(row):
+    return [row[i+5] - row[i] if i+5 < len(row) else None for i in range(len(row))]
 
-start_year = '1960'
-end_year = '2021'
+# Function to create the line chart for a selected country
+def plot_growth_rate(country_name):
+    country_data = data[data['Country Name'] == country_name].values[0]
+    years = data.columns[2:-1]
+    growth_rate = country_data[2:-1]
+    five_year_diff = calculate_5_year_differences(growth_rate)
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(years, growth_rate, marker='o', label='Growth Rate')
+    plt.plot(years[4:], five_year_diff[4:], marker='o', label='5-Year Difference')
+    plt.xlabel('Year')
+    plt.ylabel('Growth Rate')
+    plt.title(f'Growth Rate for {country_name}')
+    plt.xticks(rotation=45)
+    plt.legend()
+    st.pyplot(plt)
 
-# Step 2: Read the CSV data
-df = pd.read_csv("World_Muslim_Population _Dataset.csv")
+# Main Streamlit app
+def main():
+    st.title('Country Growth Rate Visualization')
+    
+    # Dropdown to select the country
+    country_list = data['Country Name'].unique().tolist()
+    selected_country = st.selectbox('Select a country', country_list)
+    
+    # Display the line chart for the selected country
+    plot_growth_rate(selected_country)
 
-# Step 3: Calculate the growth rate for each country
-def calculate_growth_rate(row):
-    initial_value = row[start_year]
-    final_value = row[end_year]
-    return ((final_value - initial_value) / initial_value) * 100
-
-# Step 4: Apply the function to create a new column for the growth rate
-df['Growth_Rate'] = df.apply(calculate_growth_rate, axis=1)
-
-# Step 5: Sort the DataFrame by 'Growth_Rate' in descending order and select the top 20 countries
-top_20_growth_countries = df.nlargest(20, 'Growth_Rate')
-
-# Step 6: Create the bar chart race
-bcr.bar_chart_race(
-    df=top_20_growth_countries,
-    filename='bar_chart_race.mp4',  # Output filename (change as needed)
-    orientation='h',               # Horizontal orientation for bars
-    sort='desc',                   # Sort bars in descending order
-    n_bars=10,                     # Number of bars to display at a time
-    title='Top 20 Countries with Highest Growth Rates',  # Title for the chart
-    label_bars=True,               # Show labels for bars
-    steps_per_period=10            # Number of steps (frames) for each period
-)
+if __name__ == '__main__':
+    main()
